edfeee36b8a893abef8e074b29013a8e93b6d8fe
Александр <shura.sidorenko.2018@bk.ru>
2021-11-24 19:46:05 +0300

    asd

--- js/mobile_menu.js
+++ js/mobile_menu.js
@@ -9,22 +9,32 @@ let menu = document.querySelector(".block_mob_menu");
 btn_open.addEventListener("click", open)
 btn_close.addEventListener("click", close)
 function open(){
-    menu.classList.remove("false_menu");
+    
     menu.classList.add("menu_active");
     setTimeout(function(){
-        menu.style.right = 0;
+        menu.style.right = 0 + "px";
         menu.style.opacity = 1;
+        
     },490)
+    
+    setTimeout(function(){
+       menu.classList.remove("menu_active"); 
+    },1000)
 }
 
 function close(){
-    menu.classList.remove("menu_active");
+    
     menu.classList.add("false_menu");
     
     setTimeout(function(){
-        menu.style.right = -200 + "px";
+        menu.style.right = -400 + "px";
         menu.style.opacity = 0;
+        
     },500)
+    
+    setTimeout(function(){
+       menu.classList.remove("false_menu"); 
+    },1000)
 }
 
 
